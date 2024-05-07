b60a56cf4154e7e7a052d9af480cc0a49fa5fb2b
Margarita Saman <ritasaman3@gmail.com>
2024-05-07 09:19:39 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a href='https://calendly.com/ritasaman3'>Book call</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
